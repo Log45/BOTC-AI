ceeeb0cbfe8e0d483b69db1ee46b6b64b5bd817b
Log45 <Skyker604@gmail.com>
2023-03-17 01:42:25 -0400

    idea

--- BOTC_openai.py
+++ BOTC_openai.py
@@ -7,6 +7,7 @@ def record_question():
     seconds = int(input("How long do you want to ask your question? (In seconds): ").strip())  # Duration of recording
     print("Beginning Recording...")
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    # We could create a global time limit for recording time and use sd.stop() in order to make a recording shorter than the time limit (it will be inputted by a user probably through a button press).
     sd.wait()  # Wait until recording is finished
     print("Ending Recording...")
     write('output.wav', fs, myrecording)  # Save as WAV file 
